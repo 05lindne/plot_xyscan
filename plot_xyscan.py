3a6e27efb39dabe4f6af74cb3ef4d9034da808a6
05lindne <sarahkoala@live.com>
2016-03-17 14:19:43 +0100

    argparse for color limits

--- plot_xyscan.py
+++ plot_xyscan.py
@@ -9,41 +9,54 @@ import numpy as np
 import matplotlib.pyplot as plt
 from sys import argv
 import pickle # save plots in pickle format which can be opened in interactive window
+import argparse
+import filename_handling
+import argparse
 
-current_script, my_file = argv 	# get the arguments from the command line
-								# by default, the executing script is also passed to
-								# argv; to seperate the inpu filename, the current 
-								# script is stored in a dummy variable
-
-my_output = my_file.replace('.txt', '')
-
-print"Plotting file: %r" % my_file
-
-data = np.loadtxt( my_file, delimiter="\t")
-
-ax = plt.subplot(111)
-
-plt.gca().invert_yaxis()
+# get arguments from command line
+parser = argparse.ArgumentParser()
+parser.add_argument('in_file', help = 'input filename')
+parser.add_argument('-lim', nargs = 2, type=int, help = 'lower and upper limits for color coding') 
+args = parser.parse_args() 
 
 
-plt.imshow(data, cmap='viridis', interpolation='nearest', origin='upper')
-# plt.colorbar(shrink=.92)
+def main():
 
-plt.autoscale(tight = True) # suppress white space in plot
+	in_filestub = filename_handling.filestub( args.in_file )
 
-cbar = plt.colorbar(orientation = 'vertical',format = '%.2e', shrink=.92) 
-cbar.ax.tick_params(labelsize=15) 
+	print"Plotting file: %r" % args.in_file
+
+	data = np.loadtxt( args.in_file, delimiter="\t")
+
+	ax = plt.subplot(111)
+
+	plt.gca().invert_yaxis()
+
+	if not (args.lim):
+		plt.imshow(data, cmap='viridis', interpolation='nearest', origin='upper')
+	if (args.lim):
+		plt.imshow(data, cmap='viridis', interpolation='nearest', origin='upper', vmin=args.lim[0], vmax=args.lim[1])
+	# plt.colorbar(shrink=.92)
+
+	plt.autoscale(tight = True) # suppress white space in plot
+
+	cbar = plt.colorbar(orientation = 'vertical',format = '%.2e', shrink=.92) 
+	cbar.ax.tick_params(labelsize=15) 
 
 
-plt.title(my_output, fontsize = 23)
-plt.xlabel('X-axis (pixel)', fontsize = 20)
-plt.ylabel('Y-axis (pixel)', fontsize = 20)
-plt.tick_params(axis = 'both', labelsize = 15)
+	plt.title(in_filestub, fontsize = 23)
+	plt.xlabel('X-axis (pixel)', fontsize = 20)
+	plt.ylabel('Y-axis (pixel)', fontsize = 20)
+	plt.tick_params(axis = 'both', labelsize = 15)
 
-# plt.tight_layout() # suppress chopping off labels
+	# plt.tight_layout() # suppress chopping off labels
 
-plt.savefig( (my_output +'.png'))
+	plt.savefig( (in_filestub +'.png'))
 
-pickle.dump(ax, file((my_output +'.pickle'), 'w'))
+	pickle.dump(ax, file((in_filestub +'.pickle'), 'w'))
 
-# plt.show()
+	# plt.show()
+
+
+if __name__ == '__main__':
+    main()
